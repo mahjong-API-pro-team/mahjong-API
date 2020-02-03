454948dfe427800f203fba5d086ec41101b47fde
kashiwagi-wataru <kashiwagiwataru224@gmail.com>
2020-02-03 18:33:46 +0900

    Initial Commit

---
+++ mahjongAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mahjongAPI
+//
+//  Created by 柏木航 on 2020/02/03.
+//  Copyright © 2020 柏木航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
